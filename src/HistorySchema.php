2249c08afd33c3363b406cf6b5497bdebf439157
Mustapha Ben Chaaben <mustapha3892@gmail.com>
2016-02-15 16:15:16 +0100

    Created test class

---
+++ src/HistorySchema.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Visionerp\HistorySchema;
+
+class HistorySchema
+{
+    /**
+     * Test
+     *
+     * @return void
+     */
+    public function test()
+    {
+        dd('test');
+    }
+
+}
