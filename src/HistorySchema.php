f337703d7300e53bf09c7f99d20ac672346ff637
Mustapha Ben Chaaben <mustapha3892@gmail.com>
2016-02-18 16:44:17 +0100

    Removed unrelated functions from HistorySchema

--- src/HistorySchema.php
+++ src/HistorySchema.php
@@ -84,17 +84,14 @@ class HistorySchema
     }
 
     /**
-     * Test
-     *
-     * @return void
-     */
-    public function createTableWithHistory($table, callable $function)
-    {
-        dd('test');
-    }
-
-    /**
+     * Create History reference
      * 
+     * @param Object References table to modify
+     * @param String view name
+     * 
+     * @return void
+     * 
+     * @author Mustapha Ben Chaaben
      */
     private function createHistoryReference($table, $view) {
         $table->integer('prev_id')->unsigned()->nullable();
@@ -193,38 +190,5 @@ class HistorySchema
             \Schema::drop("${view}_trash");
             \Schema::drop("${view}_history");
         });
-    }
-
-    /**
-     * Create the get all columns except procedure
-     *
-     * @return void
-     * @author Mustapha Ben Chaaben
-     **/
-    public function createGetAllColumnsExceptProcedure()
-    {
-        \DB::unprepared("
-            CREATE OR REPLACE FUNCTION get_columns_except(input_table_name TEXT, except_columns TEXT[] = ARRAY []::TEXT[], prefix TEXT = 't') RETURNS TEXT AS $$
-            BEGIN
-                RETURN array_to_string(ARRAY(SELECT prefix || '.' || c.column_name
-                    FROM information_schema.columns As c
-                        WHERE table_name = input_table_name 
-                        AND c.table_schema = current_schema()
-                        AND  c.column_name != all(except_columns)
-                ), ',');
-            END
-            $$ LANGUAGE plpgsql;
-        ");
-    }
-
-    /**
-     * Drop get all columns except procedure
-     *
-     * @return void
-     * @author Mustapha Ben Chaaben
-     **/
-    public function dropGetAllColumnsExceptProcedure()
-    {
-        \DB::unprepared("DROP FUNCTION IF EXISTS get_columns_except(TEXT, TEXT[], TEXT);");
     }
 }
